d3bd6349230ea29ced7d5b1e3ebe9a4db8cad2db
Yamil Villegas <YTorres87@gmail.com>
2022-01-19 17:55:23 -0500

    React Weather app Set Up

---
+++ src/modules/CityComponent.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+const CityComponent = () => {
+    return "City Component"
+}
+
+export default CityComponent;
---
+++ src/modules/WeatherComponent.js
@@ -0,0 +1,5 @@
+const WeatherComponent = () => {
+    return "Weather Info Component";
+}
+
+export default WeatherComponent;
